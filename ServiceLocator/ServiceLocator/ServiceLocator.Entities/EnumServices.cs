cc17ccf0a157cf337641d92a2892881eda9bfec7
mashaOtstavnova <machaotstavnova@mail.ru>
2017-04-19 11:52:55 +0500

    add edit test in first master view

---
+++ ServiceLocator/ServiceLocator/ServiceLocator.Entities/EnumServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLocator.Entities
+{
+    public static class ServicesCategory
+    {
+        public static String IT { get { return "IT, интернет, телеком"; } }
+        public static String Household { get { return "Бытовые услуги"; } }
+        public static String Business { get { return "Деловые услуги"; } }
+        public static String Art { get { return "Искусство"; } }
+        public static String Beauty { get { return "Красота, здоровье"; } }
+        public static String Courier { get { return "Курьерские поручения"; } }
+        public static String Hour { get { return "Мастер на час"; } }
+        public static String Nurses { get { return "Няни, сиделки"; } }
+        public static String Training { get { return "Обучение, курсы"; } }
+        public static String Security { get { return "Охрана, безопасность"; } }
+        public static String Power { get { return "Питание"; } }
+        public static String Holidays { get { return "Праздники, мероприятия"; } }
+        public static String Advertising { get { return "Реклама, полиграфия"; } }
+        public static String Repair { get { return "Ремонт и обслуживание техники"; } }
+        public static String Construction { get { return "Ремонт, строительство"; } }
+        public static String Garden { get { return "Сад, благоустройство"; } }
+        public static String Transport { get { return "Транспорт, перевозки"; } }
+        public static String Cleaning { get { return "Уборка"; } }
+        public static String Installation { get { return "Установка техники"; } }
+        public static String Animal { get { return "Уход за животными"; } }
+        public static String Photo { get { return "Фото- и видеосъёмка"; } }
+        public static String Other { get { return "Другое"; } }
+    }
+    
+}
